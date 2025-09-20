c8e599b1fe956bea6588d074699386ef45be0737
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-09-21 01:19:16 +0530

    Added - Initial Spring boot app

---
+++ src/test/java/toDoListApp/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package toDoListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
